2c66d74da3e16910859e122bdc258a1eeea74b54
Isto Barton <istobarton@gmail.com>
2017-03-17 10:24:39 -0500

    rollback -- need to determine deployment webpack build

--- app/index.js
+++ app/index.js
@@ -3,10 +3,10 @@ import coreHTML from './index.js';
 function component () {
     var element = document.createElement('div');
 
-    element.innerHTML = `
-    <h2>We're working on a redesign. Revelation soon!<h2>
-    <img src='./assets/compere.jpg'>
-`;
+//     element.innerHTML = `
+//     <h2>We're working on a redesign. Revelation soon!<h2>
+//     <img src='./assets/compere.jpg'>
+// `;
 
     return element;
 }
