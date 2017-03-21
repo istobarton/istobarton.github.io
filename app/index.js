aa069b0531c364acd1cdebb6d40c34512d8c18eb
Isto Barton <istobarton@gmail.com>
2017-03-20 19:25:53 -0500

    check materialize

--- app/index.js
+++ app/index.js
@@ -2,7 +2,17 @@ function component () {
     var element = document.createElement('div');
 
     element.innerHTML = `
-    <h2>We're working on a redesign. Revelation soon!<h2>
+
+    <nav>
+        <div class="nav-wrapper">
+          <ul id="nav-mobile" class="right hide-on-med-and-down">
+            <li><a href="sass.html">Sass</a></li>
+            <li><a href="badges.html">Components</a></li>
+            <li><a href="collapsible.html">JavaScript</a></li>
+          </ul>
+        </div>
+    </nav>
+    <h2>We're working on a redesign. Revelation soon!!<h2>
     <img src='./assets/compere.jpg'>
 `;
 
